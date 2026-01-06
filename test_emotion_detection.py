492931295ba53e00192981b45f991d246236fc26
Mubshira <mubshiraiqra786@gmail.com>
2026-01-06 11:28:54 -0500

    Add packaged EmotionDetection module, Flask server, and unit tests

---
+++ test_emotion_detection.py
@@ -0,0 +1,21 @@
+import unittest
+from EmotionDetection import emotion_detector
+
+
+class TestEmotionDetector(unittest.TestCase):
+    def test_emotions(self):
+        test_cases = [
+            ("I am glad this happened", "joy"),
+            ("I am really mad about this", "anger"),
+            ("I feel disgusted just hearing about this", "disgust"),
+            ("I am so sad about this", "sadness"),
+            ("I am really afraid that this will happen", "fear"),
+        ]
+
+        for text, expected in test_cases:
+            result = emotion_detector(text)
+            self.assertEqual(result["dominant_emotion"], expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
